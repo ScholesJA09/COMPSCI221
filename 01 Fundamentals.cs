9ffecf3801829dad5621478f344bac25bbc49087
ScholesJA09 <jacob9scholes@gmail.com>
2026-09-11 17:51:58 -0500

    Add project files.

---
+++ 01 Fundamentals.cs
@@ -0,0 +1,65 @@
+﻿class Fundamentals //Same name as file
+{
+    static void Main() //Main must be capitalized
+    {
+        Console.WriteLine("Hello, World!"); //Printing out
+
+        byte a;
+        bool b; //1 byte
+        char c; //2 bytes
+        short d; //2 bytes
+        int e; //4 bytes
+        float f; //4 bytes
+        long g; //8 bytes
+        double h; //8 bytes
+
+        sbyte i; //signed byte
+        ushort j; //unsigned short
+        uint k; //unsigned int
+        ulong l; //unsigned long
+
+        string m;
+
+        string name = "Jacob";
+        int age = 21;
+        double salary = 55000;
+        double percent = 0.44;
+
+        Console.WriteLine($"Hello {name}, you are {age} years old. Your salary is {salary:N}."); //Preferred string interperlation & number formatter
+        Console.WriteLine($"{percent:P1}"); //Percent formatter
+
+        name = name + " Scholes";
+
+        name = name.ToUpper(); //Uppercase method
+        Console.WriteLine(name);
+        name = name.ToLower(); //Lowercase method
+        Console.WriteLine(name);
+
+        Console.WriteLine(name.Length); //Length property
+
+        //User Input
+        Console.Write("Enter your name: "); //Enter on same line
+        string newName = Console.ReadLine();
+
+        Console.Write("Enter your age: ");
+        int newAge = int.Parse(Console.ReadLine()); //Parsing a string to an int
+
+        Console.WriteLine($"Hello {newName}, you are {newAge} years old.");
+
+        //Functions/Methods
+        DoSomething();
+
+        //Arrays
+        int[] array = new int[5]; //Empty
+        int[] array2 = [1, 2, 3, 4, 5]; //Know what to put in
+
+        for (int i = 0; i < array.Length; i++)
+        {
+            Console.WriteLine(array[i]);
+        }
+    }
+    static void DoSomething()
+    {
+
+    }
+}
---
+++ LE1.cs
@@ -0,0 +1,65 @@
+﻿class LE1
+{
+    static void Main()
+    {
+        //Task 1
+        Console.WriteLine("Hello World!");
+
+        //Task 2
+        string name = "Ethan Jones";
+        int age = 27;
+        string jobTitle = "Computer Programmer";
+        int salary = 80000;
+        bool isManager = false;
+
+        //Task 3
+        Console.WriteLine($"{name} is a {age}-year old {jobTitle} who makes ${salary:N} per year.");
+
+        if (isManager == true)
+        {
+            Console.WriteLine("They are a manager.");
+        }
+        else
+        {
+            Console.WriteLine("They are not a manager.");
+        }
+
+        //Task 4
+        double[] hours = [8.0, 7.5, 8.5, 8.0, 4.0, 0.0, 0.0];
+
+        //Task 5
+        double totalHoursWorked = 0;
+        for (int i = 0; i < hours.Length; i++)
+        {
+            totalHoursWorked += hours[i];
+        }
+
+        Console.WriteLine($"{name} worked a total of {totalHoursWorked:N2} hours this week.");
+
+        //Task 6
+        double averageHours = totalHoursWorked / 7;
+        Console.WriteLine($"{name} worked an average of {averageHours:N2} hours this week.");
+
+        //Task 7
+        Console.Write("Enter your name: ");
+        string userName = Console.ReadLine();
+        Console.Write("Enter your age: ");
+        int userAge = int.Parse(Console.ReadLine());
+
+        //Task 8
+        if (userAge < 18 || userAge > 100)
+        {
+            Console.WriteLine("Invalid age entered. Please enter an age between 18 and 100.");
+            throw new InvalidOperationException("Invalid operation!");
+        }
+
+        //Task 10
+        greetingMessage(name, age);
+        greetingMessage(userName, userAge);
+    }
+    //Task 9
+    static void greetingMessage(string name, int age)
+    {
+        Console.WriteLine($"Hello, {name}! You are {age} years old!");
+    }
+}
