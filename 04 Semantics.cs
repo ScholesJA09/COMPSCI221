36e22f32179177676cd3c9d755f83e45a9e5070e
ScholesJA09 <jacob9scholes@gmail.com>
2026-09-24 22:02:31 -0500

    LE3

---
+++ 04 Semantics.cs
@@ -0,0 +1,123 @@
+﻿namespace COMPSCI221
+{
+    static class _04_Semantics
+    {
+        static void Main()
+        {
+            int a = 4; //Value Semantics
+            int b = a; //Make a copy
+            Console.WriteLine($"a = {a}, b = {b}");
+            b = 5; //Does not modify the original
+            Console.WriteLine($"a = {a}, b = {b}");
+
+            Wrapper w1 = new Wrapper(4); //Classes use Reference Semantics
+            Wrapper w2 = w1; //w2 points to same location as w1, both are a reference to where the object of x=4 is
+            Console.WriteLine($"w1.X = {w1.X}, w2.X = {w2.X}");
+            w2.X = 5; //Any changes to actual value will affect both
+            Console.WriteLine($"w1.X = {w1.X}, w2.X = {w2.X}");
+
+            int x = 44;
+            Console.WriteLine($"\nBefore Value Semantics, x = {x}");
+            ValueSemantics(x); 
+            Console.WriteLine($"After Value Semantics, x = {x}");
+
+            Wrapper w3 = new Wrapper(44);
+            Console.WriteLine($"\nBefore Reference Semantics, x = {w3.X}");
+            ReferenceSemantics(w3);
+            Console.WriteLine($"After Reference Semantics, x = {w3.X}");
+
+            Wrapper w4 = new Wrapper(44);
+            Console.WriteLine($"\nBefore Pass By Value, x = {w4.X}");
+            PassByValue(w4);
+            Console.WriteLine($"After Pass By Value, x = {w4.X}");
+
+            Wrapper w5 = new Wrapper(44);
+            Console.WriteLine($"\nBefore Pass By Reference, x = {w5.X}");
+            PassByReference(ref w5);
+            Console.WriteLine($"After Pass By Reference, x = {w5.X}");
+
+            int y = 44;
+            Console.WriteLine($"\nBefore Pass By Reference, x = {y}");
+            PassByReference(ref y);
+            Console.WriteLine($"After Pass By Reference, x = {y}");
+
+            Point p1 = new Point(3, 4);
+            Console.WriteLine($"\nPoint p1 : ({p1.X}, {p1.Y})");
+
+            Point p2 = p1;
+            Console.WriteLine($"Point p2 : ({p2.X}, {p2.Y})");
+
+            p2.X = 5; //p1 remains uncahnged
+            Console.WriteLine($"Point p1 : ({p1.X}, {p1.Y})");
+            Console.WriteLine($"Point p2 : ({p2.X}, {p2.Y})");
+        }
+
+        static void ValueSemantics(int x)
+        {
+            Console.WriteLine($"Inside of Value Semantics, x = {x}");
+            x = 99; //Changes inside that function are local to that function
+            Console.WriteLine($"Inside of Value Semantics, x = {x}");
+        }
+
+        static void ReferenceSemantics(Wrapper w)
+        {
+            Console.WriteLine($"Inside of Reference Semantics, x = {w.X}");
+            w.X = 99; //Since we are passing a copy of the memory address, changes made in this function will affect the original
+            Console.WriteLine($"Inside of Reference Semantics, x = {w.X}");
+        }
+
+        static void PassByValue(Wrapper w)
+        {
+            Console.WriteLine($"Inside Pass By Value, x = {w.X}");
+            w = new Wrapper(99); //w now points to x = 99, but w4 still points to the original x = 44
+            Console.WriteLine($"Inside Pass By Value, x = {w.X}");
+        }
+
+        static void PassByReference(ref Wrapper w) //Passing a copy of a memory address of a memory address
+        {
+            Console.WriteLine($"Inside of Pass By Reference, x = {w.X}");
+            w = new Wrapper(99); //Overidden memory address of a memory address with a new memory address
+            Console.WriteLine($"Inside of Pass By Reference, x = {w.X}");
+        }
+
+        static void PassByReference(ref int x) //ref = pass address of
+        {
+            Console.WriteLine($"Inside of Pass By Reference, x = {x}");
+            x = 99;
+            Console.WriteLine($"Inside of Pass By Reference, x = {x}");
+        }
+
+        class Wrapper
+        {
+            public Wrapper(int x)
+            {
+                X = x;
+            }
+            public int X { get; set; }
+        }
+
+        struct Point //Uses Value Semantics, Use when values aren't modifiable often
+        {
+            public Point(int x, int y)
+            {
+                X = x;
+                Y = y;
+            }
+
+            public int X { get; set; }
+            public int Y { get; set; }
+        }
+
+        readonly struct ReadOnlyPoint
+        { 
+            public ReadOnlyPoint(int x, int y)
+            {
+                X = x;
+                Y = y;
+            }
+
+            public int X { get; } //No set accessors, incapable of modification
+            public int Y { get; }
+        }
+    }
+}
---
+++ LE3.cs
@@ -0,0 +1,105 @@
+﻿namespace COMPSCI221
+{
+    internal class LE3
+    {
+        static void Main()
+        {
+            //Task 1/5
+            double[] grades = [95.5, 72.6, 88.3, 62.9, 89.9, 70.4, 79.8, 97.1];
+            Console.WriteLine($"{string.Join(", ", grades)}");
+            Console.WriteLine($"\nAverage: {GetAverage(grades):F2}");
+            char[] chars = ToLetterGrades(grades);
+
+            Console.WriteLine("\nScores:");
+            for (int i = 0; i < grades.Length; i++)
+            {
+                Console.WriteLine($"{grades[i]} : {chars[i]}");
+            }
+
+            Console.WriteLine("\nApplying curve...");
+            ApplyCurve(grades, 1.02);
+
+            //Task 5/5
+            for (int i = grades.Length - 1; i > -1; i--)
+            {
+                Console.Write($"{grades[i]:F2}, ");
+            }
+        }
+
+        //Task 2/5
+        static double GetAverage(double[] scores)
+        {
+            double sum = 0;
+            int count = 0;
+
+            if (scores == null || scores.Length == 0)
+            {
+                throw new Exception("Array can't be null or be 0 length.");
+            }
+
+            for (int i = 0; i < scores.Length; i++)
+            {
+                sum += scores[i];
+                count++;
+            }
+
+            return sum / count;
+        }
+
+        //Task 3/5
+        static char[] ToLetterGrades(double[] scores)
+        {
+            char[] chars;
+            chars = new char[scores.Length];
+
+            if (scores == null || scores.Length == 0)
+            {
+                throw new Exception("Array can't be null or be 0 length.");
+            }
+
+            for (int i = 0; i < scores.Length; i++)
+            {
+                if (scores[i] > 100)
+                {
+                    throw new Exception("No score can't be greater than 100.");
+                }
+                else if (scores[i] >= 90)
+                {
+                    chars[i] = 'A';
+                }
+                else if (scores[i] >= 80)
+                {
+                    chars[i] = 'B';
+                }
+                else if (scores[i] >= 70)
+                {
+                    chars[i] = 'C';
+                }
+                else if (scores[i] >= 61)
+                {
+                    chars[i] = 'D';
+                }
+                else
+                {
+                    chars[i] = 'F';
+                }
+            }
+
+            return chars;
+        }
+
+        //Task 4/5
+        static void ApplyCurve(double[] scores, double multiplier)
+        {
+            if (scores == null || scores.Length == 0)
+            {
+                throw new Exception("Array can't be null or be 0 length.");
+            }
+
+            for (int i = 0; i < scores.Length; i++)
+            {
+                scores[i] = scores[i] * multiplier;
+            }
+        }
+    }
+}
